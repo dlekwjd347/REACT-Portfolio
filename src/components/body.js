48483cd55989f84b682d5bab0c337d827b1b15dd
Elizabeth Lee <dlekwjd347@gmail.com>
2020-11-13 17:51:40 -0500

    fixed parallax effect

--- src/components/body.js
+++ src/components/body.js
@@ -8,10 +8,31 @@ function body() {
         <>
            
             <Parallax strength={200}>
-                <Background className="bodyimg">
-                <img src="https://www.moma.org/media/W1siZiIsIjI0Njc4NCJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MjAwMFx1MDAzZSJdXQ.jpg?sha=3a1ed95974c1c0be" alt="monet_waterlillies" />
+                <Background className="bodyimg custom-bg">
+                <img src={require("../images/monet_waterlillies.jpg")} alt="monet_waterlillies" />
                 </Background>
             </Parallax>
+
+           
+    <Parallax
+        bgImage="/path/to/another/image"
+        renderLayer={percentage => (
+            <div
+                style={{
+                    position: 'absolute',
+                    background: `rgba(255, 125, 0, ${percentage * 1})`,
+                    left: '50%',
+                    top: '50%',
+                    width: percentage * 500,
+                    height: percentage * 500,
+                }}
+            />
+        )}
+    >
+    
+    </Parallax>
+
+
         </>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,84 +2,106 @@ import React from 'react';
 import Typed from "../components/typed";
 import Header from "../components/header";
 import Footer from "../components/footer";
-import BodyImg from "../components/body";
+// import BodyImg from "../components/body";
+import { Parallax, Background } from "react-parallax";
 
 function Home() {
   return (
     <>
-    <BodyImg />
-    <Header />
-   
-      <div data-spy="scroll" data-target=".site-navbar-target" data-offset="300" />
-      <div className="site-wrap" id="home-section">
-        <div className="site-mobile-menu site-navbar-target">
-          <div className="site-mobile-menu-header">
-            <div className="site-mobile-menu-close mt-3">
-              <span className="icon-close2 js-menu-toggle"></span>
-            </div>
-          </div>
-          <div className="site-mobile-menu-body"></div>
-        </div>
+      <Header />
+      <Parallax strength={700}>
+        <Background className="custom-bg">
+          <div
+            style={{
+              height: 2000,
+              width: "100vw",
+              backgroundRepeat: "no-repeat",
+              // backgroundPosition: "center",
+              // position: "fixed",
+              // top: "25vh",
+              marginTop: "25vh",
+              backgroundImage: "url('https://www.moma.org/media/W1siZiIsIjI0Njc4NCJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MjAwMFx1MDAzZSJdXQ.jpg?sha=3a1ed95974c1c0be')"
+            }}
+          />
+        </Background>
+        <div>
 
-        <div className="ftco-blocks-cover-1">
-          <div className="site-section-cover">
-            <div className="container">
-              <div className="row align-items-center justify-content-center">
-                <div className="col-lg-8">
-                  <h1 className="lead" data-aos="fade-up">Hi, I'm Liz. I create with <Typed /></h1>
-                  <p data-aos="fade-up" data-aos-delay="100">
-                    <a className="more-29291" href="/Elizabeth_Lee_Resume.pdf" target="_blank" id="resume">View Resume</a></p>
+
+          <div data-spy="scroll" data-target=".site-navbar-target" data-offset="300" />
+          <div className="site-wrap" id="home-section">
+            <div className="site-mobile-menu site-navbar-target">
+              <div className="site-mobile-menu-header">
+                <div className="site-mobile-menu-close mt-3">
+                  <span className="icon-close2 js-menu-toggle"></span>
                 </div>
               </div>
+              <div className="site-mobile-menu-body"></div>
             </div>
-          </div>
-        </div>
-        <div className="site-section">
 
-          <div className="site-section">
-            <div className="container" id="aboutMe">
-              <div className="row justify-content-center">
-                <div className="col-md-9">
-                  <h2 className="text-primary mb-6 font-weight-bold portheading">About me</h2>
-                  <div className="d-md-flex testimony-29101 align-items-stretch portfoliocard animate__animated animate__slideInUp">
-                    <div className="image" style={{ backgroundImage: `url("../assets/images/me3.jpg")` }}></div>
-                    <div className="text">
-                      <blockquote>
-                        <p>Learning how to code is my new-found passion. My field of expertise includes HTML, CSS, Bootstrap, Javascript, and React. With these skills, I provide efficient solutions to clients globally.</p>
-                        <p>I am currently employed as a Financial Crimes Consultant at K2 Intelligence. I provide support to financial institutions like banks and hedge funds to strengthen their Anti-Money Laundering policies and procedures by leveraging advanced screening systems and subject matter expertise.</p>
-                        <div className="author">&mdash; Elizabeth Lee</div>
-                      </blockquote>
+            <div className="ftco-blocks-cover-1">
+              <div className="site-section-cover">
+                <div className="container">
+                  <div className="row align-items-center justify-content-center">
+                    <div className="col-lg-8">
+                      <h1 className="lead" data-aos="fade-up">Hi, I'm Liz. I create with <Typed /></h1>
+                      <p data-aos="fade-up" data-aos-delay="100">
+                        <a className="more-29291" href="/Elizabeth_Lee_Resume.pdf" target="_blank" id="resume">View Resume</a></p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
- 
-          <div className="site-section">
-            <div className="container">
-              <div className="row mb-4 text-center">
-                <div className="col">
-                  <a href="https://www.linkedin.com/in/elizabeth-lee2018/"><span className="m-2 icon-linkedin"></span></a>
-                  <a href="#"><span className="m-2 icon-instagram"></span></a>
+            <div className="site-section">
+
+              <div className="site-section">
+                <div className="container" id="aboutMe">
+                  <div className="row justify-content-center">
+                    <div className="col-md-9">
+                      <h2 className="text-primary mb-6 font-weight-bold portheading">About me</h2>
+                      <div className="d-md-flex testimony-29101 align-items-stretch portfoliocard animate__animated animate__slideInUp">
+                        <div className="image" style={{ backgroundImage: `url("../assets/images/me3.jpg")` }}></div>
+                        <div className="text">
+                          <blockquote>
+                            <p>Learning how to code is my new-found passion. My field of expertise includes HTML, CSS, Bootstrap, Javascript, and React. With these skills, I provide efficient solutions to clients globally.</p>
+                            <p>I am currently employed as a Financial Crimes Consultant at K2 Intelligence. I provide support to financial institutions like banks and hedge funds to strengthen their Anti-Money Laundering policies and procedures by leveraging advanced screening systems and subject matter expertise.</p>
+                            <div className="author">&mdash; Elizabeth Lee</div>
+                          </blockquote>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="row mt-5 justify-content-center">
-                <div className="col-md-7 text-center">
+
+              <div className="site-section">
+                <div className="container">
+                  <div className="row mb-4 text-center">
+                    <div className="col">
+                      <a href="https://www.linkedin.com/in/elizabeth-lee2018/"><span className="m-2 icon-linkedin"></span></a>
+                      <a href="#"><span className="m-2 icon-instagram"></span></a>
+                    </div>
+                  </div>
+                  <div className="row mt-5 justify-content-center">
+                    <div className="col-md-7 text-center">
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
+            <Footer />
+
+
+
+
+
           </div>
+          <div />
         </div>
-      
-        <Footer />
+      </Parallax>
+      {/* <BodyImg /> */}
 
-
-
-
-
-      </div>
-      <div />
+      {/* </Parallax> */}
     </>)
 
 }
